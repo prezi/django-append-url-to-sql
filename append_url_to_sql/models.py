27a4b3abf5292adabb26fc62900e599e389e951d
Peter Neumark <peter.neumark@prezi.com>
2013-05-07 10:55:42 +0200

    Added the ability to sql sql_query_tag local variables which will be used instead of the request URL in the SQL comment

--- append_url_to_sql/models.py
+++ append_url_to_sql/models.py
@@ -57,15 +57,26 @@ from django.http import HttpRequest
 from django.db.backends import util, BaseDatabaseWrapper
 
 class CursorWrapper(util.CursorDebugWrapper):
+    def get_sql_query_tag_message(self, f_locals):
+        message = f_locals.get('sql_query_tag')
+        return repr(message) if message is not None else message
+
+    def get_request(self, f_locals):
+        request = f.f_locals.get('request')
+        if isinstance(request, HttpRequest):
+            return repr(request.path)[2:-1].replace('%', '%%')
+        return None
+
     def execute(self, sql, *args):
         f = sys._getframe()
         while f:
-            request = f.f_locals.get('request')
-            if isinstance(request, HttpRequest):
-                sql += ' -- %s' % repr(request.path)[2:-1].replace('%', '%%')
+            sql_comment = None
+            f_locals = f.f_locals
+            log_message = self.get_sql_query_tag(f_locals) or self.get_request(f_locals)
+            if log_message is not None:
+                sql += ' -- %s' % log_message
                 break
             f = f.f_back
-
         return self.cursor.execute(sql, *args)
 
 if getattr(settings, 'APPEND_URL_TO_SQL_ENABLED', True):
